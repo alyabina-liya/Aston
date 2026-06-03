3fcc74c30599df1b244e2ad555a9028104f3386c
alyabina-liya <alyabina.liya@gmail.com>
2026-06-03 22:17:37 +0300

    Домашнее задание по массивам

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void Main(String[] args) {
+
+    }
+
+    public static void printThreeWords(){
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+}
